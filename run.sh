75b94c12e85fea9c022c152fe76da9764d123e30
Erin Maus <squeak@erinmaus.com>
2024-08-09 09:37:51 -0400

    Add release pipeline.

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+if [ -f "./.vscode/.env" ]; then
+    set -a && source ./.vscode/.env && set +a
+fi
+
 "${LOVE_BINARY:-love}" --fused .
--- tests.sh
+++ tests.sh
@@ -4,6 +4,10 @@ set -e
 
 make all
 
+if [ -f "./.vscode/.env" ]; then
+    set -a && source ./.vscode/.env && set +a
+fi
+
 if [ -z "${CI+yes}" ]; then
     "${LOVE_BINARY:-love}" --fused tests ci=yes
 else
